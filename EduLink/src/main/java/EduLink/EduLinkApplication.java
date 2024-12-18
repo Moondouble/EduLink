58b12da7815a10fca1609011adb3700cb4f085b0
Moondouble <jjm0586@sungkyul.ac.kr>
2024-12-18 10:34:48 +0900

    123132

---
+++ EduLink/src/main/java/EduLink/EduLinkApplication.java
@@ -0,0 +1,13 @@
+package EduLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduLinkApplication.class, args);
+	}
+
+}
