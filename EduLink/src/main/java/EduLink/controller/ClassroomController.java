cd4ab89b99586ddba9c9ca4096dda50775ecbb33
Moondouble <jjm0586@sungkyul.ac.kr>
2025-01-03 12:51:32 +0900

    1234570[

--- EduLink/src/main/java/EduLink/controller/ClassroomController.java
+++ EduLink/src/main/java/EduLink/controller/ClassroomController.java
@@ -11,12 +11,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import EduLink.command.BoardCommand;
 import EduLink.command.ClassroomCommand;
 import EduLink.domain.AuthInfoDTO;
+import EduLink.domain.ClassroomDTO;
+import EduLink.domain.TeacherDTO;
 import EduLink.service.AutoNumService;
 import EduLink.service.classroom.ClassroomDeleteService;
 import EduLink.service.classroom.ClassroomDetailService;
 import EduLink.service.classroom.ClassroomListService;
 import EduLink.service.classroom.ClassroomUpdateService;
 import EduLink.service.classroom.ClassroomWriteService;
+import EduLink.service.teacher.TeacherDetailService;
 import jakarta.servlet.http.HttpSession;
 
 
@@ -36,6 +39,8 @@ public class ClassroomController
 	ClassroomUpdateService classroomUpdateService;
 	@Autowired
 	ClassroomDeleteService classroomDeleteService;
+	@Autowired
+	TeacherDetailService teacherDetailService;
 	@GetMapping("classList")
 	public String List(Model model) {
 		classroomListService.execute(model);
@@ -69,6 +74,13 @@ public class ClassroomController
 	@GetMapping("classDetail")
 	public String detail(@RequestParam("classNum") String classNum,Model model) {
 		classroomDetailService.execute(classNum,model);
+		ClassroomDTO classroomCommand = (ClassroomDTO) model.getAttribute("classroomCommand");
+		String teacherNum = classroomCommand.getTeacherNum();
+		System.out.println(teacherNum);
+		teacherDetailService.execute(teacherNum, model);
+		TeacherDTO teacherCommand = (TeacherDTO) model.getAttribute("teacherCommand");
+		
+		System.out.println(teacherCommand.getTeacherStoreImage());
 		return "thymeleaf/class/classInfo";
 	}
 	
