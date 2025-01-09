9fc201f11fca1bb0c8c8392d5a39e2d26a92dda7
leehh00 <mihailxk@naver.com>
2025-01-09 17:03:13 +0900

    결제(오류수정해야됨)

---
+++ EduLink/src/main/java/EduLink/domain/PaymentDTO.java
@@ -0,0 +1,20 @@
+package EduLink.domain;
+
+import org.apache.ibatis.type.Alias;
+
+import lombok.Data;
+
+@Data
+@Alias("paymentDTO")
+public class PaymentDTO {
+	String purchaseNum;
+	String confirmNumber;
+	String cardNum;
+	String tid;
+	String totalPrice;
+	String resultMessage;
+	String payMethod;
+	String applDate;
+	String applTime;
+	String purchaseName;
+}
--- EduLink/src/main/java/EduLink/mapper/PurchaseMapper.java
+++ EduLink/src/main/java/EduLink/mapper/PurchaseMapper.java
@@ -1,8 +1,22 @@
 package EduLink.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import EduLink.domain.PaymentDTO;
+import EduLink.domain.PurchaseDTO;
+import EduLink.domain.PurchaseListDTO;
 
 @Mapper
 public interface PurchaseMapper {
 	public String selectNum();
+	public Integer purchaseInsert(PurchaseDTO dto);
+	public int purchaseListInsert(PurchaseListDTO dto);
+	public PurchaseDTO purchaseSelect(String purchaseNum);
+	public int purchaseGoodsCount(String purchaseNum);
+	public String firstGoods(String purchaseNum);
+	public int paymentInsert(PaymentDTO dto);
+	public int purchaseStatusUpdate(@Param("status") String status,
+			@Param("purchaseNum") String purchaseNum);
+	
 }
