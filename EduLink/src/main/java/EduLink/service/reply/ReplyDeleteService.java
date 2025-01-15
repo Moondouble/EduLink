fe782acf5b26c834befd5e589e4f7325ee3e32a5
Moondouble <jjm0586@sungkyul.ac.kr>
2025-01-15 13:54:11 +0900

    1212

--- EduLink/src/main/java/EduLink/service/reply/ReplyDeleteService.java
+++ EduLink/src/main/java/EduLink/service/reply/ReplyDeleteService.java
@@ -1,11 +1,13 @@
 package EduLink.service.reply;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import EduLink.mapper.ReplyMapper;
 
 @Service
 public class ReplyDeleteService {
+	@Autowired
 	ReplyMapper replyMapper;
 	public void execute(String replyNum) {
 		replyMapper.replyDelete(replyNum);
